8964555d453289f773ddfb3c5346cb37a9a59fa0
stephenmuharsky <50891974+stephenmuharsky@users.noreply.github.com>
2021-12-28 16:32:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,34 @@
 let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('.darkmode-toggle');
-var moonIcon = document.getElementById('moon');
-
-function disappear(){
-    moonIcon.style.display = "none";
-    console.log("Now you don't!")
-}
-
-function appear(){
-    moonIcon.style.display = "inline";
-    console.log("Now you see me,")
-}
 
 
 const enableDarkMode = ()=>{
-    //moonIcon.src = "./Assets/moon.png";
     document.body.classList.add('darkmode');
-    localStorage.setItem('darkMode', 'enabled');
     console.log('darkmode enabled');
 }
 
 const disableDarkMode = ()=>{
-    //moonIcon.src = "./Assets/moonAlt.png";
     document.body.classList.remove('darkmode');
-    localStorage.setItem('darkMode', null);
+    //localStorage.setItem('darkMode', null);
     console.log('darkmode disabled');
 }
 
+/* Check if darkmode is already enabled. Implements it if so. */
+if(darkMode){
+    enableDarkMode();
+    darkModeToggle.checked=true;
+}else{
+    disableDarkMode();
+    darkModeToggle.checked = false;
+}
 
 darkModeToggle.addEventListener("click", function(){
     darkMode = localStorage.getItem('darkMode');
     if(darkMode !== 'enabled'){
+        localStorage.setItem('darkMode', 'enabled');
         enableDarkMode();
-        appear();
     }else{
+        localStorage.removeItem('darkMode');
         disableDarkMode();
-        disappear();
     }
 })
-
-
-
-/*
-localStorage.clear();
-*/
-
